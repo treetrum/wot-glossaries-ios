cff2eb45df99ce29544440c0af559aeb90afad1e
Sam Davis <sam@sjd.co>
2022-03-29 20:12:31 +1100

    Initial Commit

---
+++ Shared/wot_glossariesApp.swift
@@ -0,0 +1,17 @@
+//
+//  wot_glossariesApp.swift
+//  Shared
+//
+//  Created by Samuel Davis on 29/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct wot_glossariesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
